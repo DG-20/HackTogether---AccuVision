6cce4756cd80cd633483de5f090c4e32c6249e5a
DG-20 <divyansh.goyal2001@gmail.com>
2021-01-12 21:46:17 -0700

    Tested and works

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import cv2 as cv
 import numpy as np
 
-video = cv.VideoCapture("Lake - 11571.mp4")
-isTrue, frame1 = video.read()
-isTrue, frame2 = video.read()
+video = cv.VideoCapture('City.mp4')
+ret, frame1 = video.read()
+ret, frame2 = video.read()
 
 
 while video.isOpened():
-    Difference = cv.absdiff(frame1, frame2)
-    greyScale = cv.cvtColor(Difference, cv.COLOR_BGR2GRAY)
+    difference = cv.absdiff(frame1, frame2)
+    greyScale = cv.cvtColor(difference, cv.COLOR_BGR2GRAY)
     blurred = cv.GaussianBlur(greyScale, (5,5), 0)
-    thresh = cv.threshold(blurred, 20, 255, cv.THRESH_BINARY)
-    dilated = cv.dilate(thresh, NONE, iterations = 3)
-    contours = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    _, thresh = cv.threshold(blurred, 20, 255, cv.THRESH_BINARY)
+    dilated = cv.dilate(thresh, None, iterations=3)
+    contours, _ = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     cv.drawContours(frame1, contours, -1, (0,255,0), 2)
 
     cv.imshow("TEST", frame1)
     frame1 = frame2
-    frame2 = video.read()
+    ret, frame2 = video.read()
 
     if cv.waitKey(40) == 27:
         break
 
 cv.destroyAllWindows()
+video.release()
