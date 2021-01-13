1607b26a7ed137c21abaa114f30b73e97f498709
curtissilva <curtissilva77@gmail.com>
2021-01-12 21:27:30 -0700

    Test Added Videos

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 
-video = cv.VideoCapture()
+video = cv.VideoCapture("Lake - 11571.mp4")
 isTrue, frame1 = video.read()
 isTrue, frame2 = video.read()
 
@@ -10,7 +10,7 @@ while video.isOpened():
     Difference = cv.absdiff(frame1, frame2)
     greyScale = cv.cvtColor(Difference, cv.COLOR_BGR2GRAY)
     blurred = cv.GaussianBlur(greyScale, (5,5), 0)
-    thresh = cv.threshold(blurred, 20, 255, cv.THRESh_BINARY)
+    thresh = cv.threshold(blurred, 20, 255, cv.THRESH_BINARY)
     dilated = cv.dilate(thresh, NONE, iterations = 3)
     contours = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     cv.drawContours(frame1, contours, -1, (0,255,0), 2)
