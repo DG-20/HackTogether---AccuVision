7ad0226ee93b0f8466f87c65c64a2739bad2d2f3
DG-20 <divyansh.goyal2001@gmail.com>
2021-01-16 23:16:22 -0700

    CSV

--- app.py
+++ app.py
@@ -3,18 +3,13 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 import time
-import plotly.graph_objects as go
 import plotly.express as px
 
 app = dash.Dash(__name__)
 
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
+df = pd.read_csv("data/test.csv")
 
-fig = px.line(df, x="Fruit", y="Amount", title="Stuff")
+fig = px.bar(df, x="Fruit", y="Amount", title="Stuff")
 
 app.layout = html.Div(children =
     [
@@ -24,7 +19,7 @@ app.layout = html.Div(children =
         dcc.Graph(
             figure = fig
         )
-        
+
     ])
 
 if __name__ == '__main__':
