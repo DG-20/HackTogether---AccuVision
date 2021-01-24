6b656e7cf96c5e1194c1f7831c79020d303e181b
Mohamed Yassin <moyassin2001@gmail.com>
2021-01-23 20:37:42 -0700

    Some dropdown menu progress

--- app.py
+++ app.py
@@ -4,16 +4,19 @@ import dash_html_components as html
 import pandas as pd
 import time
 import plotly.express as px
+import plotly.graph_objects as go
 
 #Started the app
 app = dash.Dash(__name__)
 
+
 #Reading data
 df = pd.read_csv("data/test.csv")
 
-#Line graph object created
-fig = px.line(df, x="Fruit", y="Amount", title="Stuff")
 
+
+#Line graph object created
+fig = px.line(df, x="Time of Day", y="Number of People Inside", title="Number of People Inside a Store at Different Times of Day" )
 app.layout = html.Div(children =
     [
         #Random HTML
@@ -30,3 +33,32 @@ app.layout = html.Div(children =
 if __name__ == '__main__':
     app.run_server(debug=True)
 
+#Creating figure
+
+fig = go.Figure()
+#Adding surface trace
+fig.add_trace(go.Surface(z= df.values.tolist(), colorscale = "tealgrn"))
+
+#Update plot sizing
+
+fig.update_layout(
+    width = 800,
+    height = 800,
+    autosize = False,
+    margin=dict(t=0, b=0, l=0, r=0),
+    template = "gridon",
+)
+
+#Adding dropdown menu
+
+fig.update_layout(
+    updatemenus = [
+        dict(
+            buttons = list([
+                dict(
+                    args = ["type", ]
+                )
+
+                )
+            ])
+        )
