24eac19e6e92cbbd79d1ce837bd9a0658df3824c
DG-20 <divyansh.goyal2001@gmail.com>
2021-01-26 17:17:24 -0700

    NO change

--- app.py
+++ app.py
@@ -9,14 +9,31 @@ import plotly.graph_objects as go
 #Started the app
 app = dash.Dash(__name__)
 
-
 #Reading data
 df = pd.read_csv("data/test.csv")
 
 #Line graph object created
+
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='demo-dropdown',
+        options=[
+            {'label': 'Sat', 'value': ' Sat'},
+            {'label': 'Sun', 'value': ' Sun'},
+            {'label': 'Mon', 'value': ' Mon'}
+        ],
+        value=' Sat'
+    ),
+    html.Div(id='dd-output-container')
+])
 fig = px.line(df, x="Time of Day", y=" Sat", title="Stuff")
-
-
+suppress_callback_exceptions=True
+@app.callback(
+    dash.dependencies.Output('dd-output-container', 'children'),
+    [dash.dependencies.Input('demo-dropdown', 'value')])
+def update_output(value):
+    fig = px.line(df, x="Time of Day", y=value, title="Stuff")
+    return 'You have selected "{}"'.format(value)
 
 app.layout = html.Div(children =
     [
@@ -27,12 +44,11 @@ app.layout = html.Div(children =
         #The actual graph to display
         dcc.Graph(
             figure = fig
-        )
-    ])
-
+        ),
+        html.Button('Sat', id = 'Sat'),
+        html.Button('Sun', id = 'Sun')
+        ])
+suppress_callback_exceptions=True
 #Running it
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-#Creating figure
-
