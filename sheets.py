156b9b040fa33d9f3eaca12f528d26617c5b6535
DG-20 <divyansh.goyal2001@gmail.com>
2021-02-16 16:27:33 -0700

    Quick check

--- sheets.py
+++ sheets.py
@@ -1,4 +1,5 @@
 import gspread as GS
+import numpy as np
 gc = GS.service_account(filename='creds.json') 
 
 sheet1 = gc.open('tutorial').sheet1
@@ -8,5 +9,8 @@ data = sheet1.get_all_records()
 
 print(data)
 
+headers = np.array["Time of Day", "Monday", "Tuesday", "Wednesday",
+                       "Thursday", "Friday", "Saturday", "Sunday"]
 
+sheet1.update('A1', headers.tolist())
 
